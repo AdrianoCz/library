80d95e4ee60616faa75271076b891b4d604853d6
Adriano <235764285+AdrianoCz@users.noreply.github.com>
2026-03-18 20:01:16 -0300

    add fully working library w/ styling

---
+++ script.js
@@ -0,0 +1,96 @@
+const myLibrary = [];
+const form = document.querySelector("form")
+
+form.addEventListener("submit", function (event) {event.preventDefault();})
+
+class Book {
+    constructor(name, pages, author, year) {
+
+        this.id = crypto.randomUUID();
+        this.name = name;
+        this.pages = pages;
+        this.author = author;
+        this.year = year;
+        this.read;
+    }
+}
+
+function addBookToLibrary() {
+    const name = document.getElementById("name").value;
+    const pages = document.getElementById("pages").value;
+    const author = document.getElementById("author").value;
+    const year = document.getElementById("year").value;
+
+    myLibrary.push(new Book(name, pages, author, year));
+    
+    clearForm();
+    displayLibrary();
+}
+
+function clearForm(){
+    const values = document.querySelectorAll(".info")
+    values.forEach(element => {element.value = ""
+    });
+}
+
+function displayLibrary() {
+
+        const library = document.querySelector(".library")
+        library.innerHTML = ""
+
+    for(let i = 0; i<myLibrary.length; i++) {
+
+        const book = document.createElement("div");
+        const name = document.createElement("h1");
+        const info = document.createElement("div");
+        const pages = document.createElement("p");
+        const author = document.createElement("p");
+        const year = document.createElement("p") ;        
+        const remove = document.createElement("button");
+        const read = document.createElement("p")
+        const readStatus = document.createElement("button");
+
+        remove.innerText = "remover";
+        remove.id = myLibrary[i].id;
+        remove.setAttribute("onclick", "removeBook(event.target.id)")
+
+        readStatus.addEventListener("click",(e)=>{toggleRead(e.target)})
+        readStatus.setAttribute("name",myLibrary[i].id)
+
+        pages.innerText = `Pages: ${myLibrary[i].pages}`;
+        author.innerText = `Author: ${myLibrary[i].author}`;
+        year.innerText = `Release year: ${myLibrary[i].year}`;
+        name.innerText = myLibrary[i].name;
+        readStatus.innerText = myLibrary[i].read ? "Mark as unread" : "Mark as Read" 
+        read.innerText = myLibrary[i].read ? "read": "not read"
+
+        info.appendChild(pages);
+        info.appendChild(author);
+        info.appendChild(year);
+        info.appendChild(read);
+        book.appendChild(name);
+        book.appendChild(info);
+        book.appendChild(remove);
+        book.appendChild(readStatus)
+        library.appendChild(book);
+    }
+}
+
+function toggleRead(id) {
+    myLibrary.forEach((element)=>{
+        if(element.id == id.name){
+            element.read = !element.read
+        }
+    })
+    displayLibrary();
+}
+
+function removeBook(id) {
+    myLibrary.forEach((element, i) => {
+        if(element.id == id) {
+            myLibrary.splice(i,1);
+        }
+
+    });
+    displayLibrary();
+}
